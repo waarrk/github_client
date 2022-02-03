640ecd23d9c6a170494c65a5b95a406f1682bfc6
waarrk <48860242+waarrk@users.noreply.github.com>
2022-02-03 16:30:38 +0900

    add: git login

---
+++ lib/github_oauth_credentials.dart
@@ -0,0 +1,6 @@
+// TODO(CodelabUser): Create an OAuth App
+const githubClientId = 'dd527a417e1b4a62fcd5';
+const githubClientSecret = '041167697aca9e6c4386a33b013f9dbb25d3450c';
+
+// OAuth scopes for repository and user information
+const githubScopes = ['repo', 'read:org'];
